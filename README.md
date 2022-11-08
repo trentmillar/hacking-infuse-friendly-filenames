ded0de21572d8b9aed2d72aa9482e9dfcf81f719
trentmillar <trent.millar@gmail.com>
2022-11-07 19:35:40 -0700

    added working renamer | added some readme details

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # hacking-infuse-friendly-filenames
-this is very ugly but saves me from manually renaming files
+I spent an afternoon getting my massive video library formatted for Infuse to index but found Infuse isn't so flexible with my naming convention. After many attempts at getting my entire library renamed only to find out hundreds of files still weren't recognized I decided to hack this out.
+This is very ugly but saves me from manually renaming files.
+
+## how to use
+Well this is fairly specific to filenames that already have some structure. 
+The all of following will get you a good name, some of the following may not
+1. your file should have the episode number, E01 or x01
+2. your file should have the season number, S01
+3. your folders above must have the show name, Magnum PI
+4. your folder(s) above should have the season number, season 1
+
+If you have most of this then you should get a tight Infuse named video,
+
+- magnum-pi_s01e03.mkv
+
+The show's folder name will be what is used for these files so if you want to help Infuses' chance of finding the show try,
+
+- add the show's originating year to the folder, Magnum PI 1980, to get,
+  - magnum-pi-1980_s01e03.mkv
+
+### how to name files
+Bone up on how Infuse expects your files to look like, [Infuse - Metadata 101](https://support.firecore.com/hc/en-us/articles/215090947-Metadata-101)
+
+### how to run the script
+1. clone this repo somewhere
+2. make sure Python 3.8 or greater is installed
+3. `cd` to the folder where the py script is
+4. run `python rename_my_files.py`
+
+(optional) if your videos are on a NAS or SMB share somewhere 
+I added some mounting logic to mount the networked folder. As it stands you will be prompted for a username & password for this share. FYI, I hard coded mine so you'll need to change this in the code.
+
+If you have the video's locally or you already have a mapped drive using Windows then you will need to gut my mounting logic and simply hard code your path in `mount_dir`
+
+All the pathing here is pegged to a Mac or POSIX machine so if you're running Windows you'll need to swap some code, like `/` for `\\`.
+
+### what is the output
+If your folder and file names are somewhat clean (see #how to use) you will get this,
+
+1. your video will be renamed (inline) to include, _show_season|episode.extension_ 
+2. a _.orignal_ file will be added in the same folder showing the original name if you want to revert
+3. any warnings or errors for this file will also be added in the folder with the filename plus _.errors.txt_
+
+The error log can give you some guidance as to why a name couldn't be used for your file.
+
+### caveat emptor
+This was built for my existing video file names so it is very likely it will not work out-of-the-box for you. I suggest you comment out any `os.rename` and `open(...)` in the script. Then run and look at the log output to see how close this is with your filenames.
--- rename_my_files.py
+++ rename_my_files.py
@@ -2,12 +2,38 @@
 
 import os
 import re
+import sys
+import stat
 
-s_and_e = re.compile(r"(s[0-9]{1,3}(\.|_){0,1}e[0-9]{1,4})|([0-9]{1,3}x[0-9]{1,4})", re.IGNORECASE)
+
+def get_expression(name, exp):
+    group = exp.search(name)
+    if group is None:
+        return None, None
+    sande = group.group()
+    all = re.findall("[0-9]+", sande)
+    if len(all) == 1:
+        return None, all[0].zfill(2)  # assuming we just have episode
+    if len(all) == 2:
+        return all[0].zfill(2), all[1].zfill(2)
+    else:
+        print("ERROR: found {0}".format(",".join(all)))
+        raise Exception("should never match more than 2 or less than 1")
 
 
-def contains_season_episode(name):
-    return s_and_e.search(name) is not None
+s_and_e = re.compile(r"(s[0-9]{1,3}(\.|_| ){0,1}e[0-9]{1,4})|([0-9]{1,3}x[0-9]{1,4})", re.IGNORECASE)
+
+
+def get_season_and_episode(name):
+    return get_expression(name, s_and_e)
+
+
+ep_and_s = re.compile(r"(s[0-9]{1,3}(\.|_| ){0,1}e[0-9]{1,4})|([0-9]{1,3}x[0-9]{1,4})|(^[0-9]{1,4}( ))|(^[0-9]{1,4}$)",
+                      re.IGNORECASE)
+
+
+def get_season_and_episode_aggressive(name):
+    return get_expression(name, ep_and_s)
 
 
 sea = re.compile(r"^((season|s){0,1}( ){0,1}[0-9])$", re.IGNORECASE)
@@ -39,14 +65,14 @@ def get_season_and_show(folder_parts):
     return show, season
 
 
-ep = re.compile(r"^(e){0,1}[0-9]{1,4}( ){0,1}", re.IGNORECASE)
+ep = re.compile(r"(^(e){0,1}[0-9]{1,4}( |.){0,1})|(( |.){1}(e){1}[0-9]{1,4}( ){0,1})", re.IGNORECASE)
 
 
 def get_episode_number(name):
     group = ep.search(name)
     if not group:
         return None
-    return group.group(0).zfill(2)
+    return re.findall("[0-9]+", group.group(0))[0].zfill(2)
 
 
 def execute_command(command):
@@ -72,31 +98,96 @@ result = execute_command(umount)
 result = execute_command(mkdir)
 result = execute_command(mount)
 
-extensions = [".mkv", ".avi", ".mp4", ".m4v"]
+extensions = [
+    ".mkv", ".avi", ".mp4", ".m4v", "flv", "iso", "img", "mov", "mpeg", "ogv", "webm"
+]
+
+
+def persist_log(msg, filename, root_path, level="error"):
+    log_file = os.path.join(root_path, "{0}.{1}.txt".format(filename, level))
+    with open(log_file, 'a') as log:
+        log.write(msg)
+
+
 for root, dirs, files in os.walk(mount_dir):
     video_files = [file for file in files if os.path.splitext(file)[1] in extensions]
     if len(video_files) == 0:
         continue
 
     folders = list(filter(None, root.replace(mount_dir, "").split("/")))
-    show, season = get_season_and_show(folders)
+    show, folder_season = get_season_and_show(folders)
 
     # This could get ugly
     for file in video_files:
         # does file match SNNENN
-        if contains_season_episode(file):
-            print("PASS-ABLE: this may be usable, {0}".format(file))
-            # We can do better if we have a show and season
-            if not show or not season:
-                continue
+        filename = str(os.path.splitext(file)[0]).replace(show, "")
+        if folder_season:
+            file_season, episode = get_season_and_episode_aggressive(filename)
+        else:
+            file_season, episode = get_season_and_episode(filename)
+        if file_season is not None and episode is not None:
+            print("\tPASS-ABLE: this may be usable, {0}".format(file))
 
-        episode = get_episode_number(file)
+        if episode is None:
+            persist_log("ERROR: missing episode from, file:{0}".format(file), file, root)
+            continue
+
+        if file_season is None and folder_season is None:
+            persist_log("ERROR: missing season from, file:{0} or folders:{1}".format(file, "/".join(folders)), file,
+                        root)
+            continue
+
+        # We can do better if we have a show and season
+        if not show:  # todo, get show from filename - now it is just from parent folder
+            persist_log("ERROR: missing show name,{0} out of {1}".format(show, file), file, root)
+            continue
+
+        # other_episode = get_episode_number(file)
+
+        if not folder_season and not file_season and folder_season != file_season:
+            err = "WARN: folder season {0} is not equal to file season {1}".format(folder_season, file_season)
+            persist_log(err, file, root, "warn")
+
+        # if episode != other_episode:
+        #     err = "WARN: episode {0} is not equal to episode {1}".format(episode, other_episode)
+        #     persist_log(err, file, root, "warn")
+
+        # Coalesce
+        season = file_season or folder_season
+        # episode = episode or other_episode
 
         if not show or not season or not episode:
-            print("ERROR: this, {0}/{1} became, {2}-s{3}_e{4}".format("/".join(folders), file, show, season, episode))
+            err = "ERROR: this, {0}/{1} became, {2}-s{3}_e{4}".format("/".join(folders), file, show, season, episode)
+            persist_log(err, file, root)
             continue
 
         extension = os.path.splitext(file)[1]
-        print("PASS: {0}_s{1}e{2}.{3}".format(show, season, episode, extension))
+
+        new_filename = "{0}_s{1}e{2}{3}".format(show, season, episode, extension)
+
+        if new_filename == file:
+            print("INFO: file name {0} already formatted".format(new_filename))
+            continue
+
+        shadow_filename = "{0}___{1}.original".format(new_filename, file)
+
+        print("INFO: renamed, {0}\t{1}".format(new_filename, file))
+        new_filepath = os.path.join(root, new_filename)
+
+        # if new file exists we have a problem
+        if os.path.isfile(new_filepath):
+            err = "ERROR: new file exists, {0}".format(new_filename)
+            persist_log(err, file, root)
+            continue  # bug out before overwriting
+
+        old_filepath = os.path.join(root, file)
+
+        if sys.platform == 'darwin':  # locked files on my POSIX
+            flags = os.stat(old_filepath).st_flags
+            if flags & stat.UF_IMMUTABLE:
+                os.chflags(old_filepath, flags & ~stat.UF_IMMUTABLE)
+
+        os.rename(old_filepath, new_filepath)
+        open(os.path.join(root, shadow_filename), 'a').close()
 
 result = execute_command(umount)
